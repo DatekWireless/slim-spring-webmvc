dca9c3fb1116c43a5778d335499b94aa75f8182b
Uwe Kubosch <uwe@datek.no>
2023-11-17 18:45:01 +0100

    Try logging SLIM initialization exceptions.

--- src/main/resources/ruby/load_slim.rb
+++ src/main/resources/ruby/load_slim.rb
@@ -2,4 +2,13 @@
 
 # This file is loaded once for every view that is instantiated.
 # It uses (requires) a separate file to avoid loading the same code multiple times.
-require 'ruby/slim_renderer'
+begin
+  require 'ruby/slim_renderer'
+rescue Exception => e
+  Java::OrgApacheCommonsLogging::LogFactory.getLog('no.datek.slim').error(e.to_s)
+
+  def render_slim(template, model_map, rendering_context)
+    "<h1>Whoops!</h1><p>Failed to load SLIM template framework.</p>"
+  end
+end
+
