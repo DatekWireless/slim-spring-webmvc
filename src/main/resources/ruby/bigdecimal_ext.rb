99fd4ea04721effb1e30310e39bf7c9b8c2192eb
Uwe Kubosch <uwe@datek.no>
2024-02-20 14:39:23 +0100

    Alias BigDecimal to_fs to to_s

--- src/main/resources/ruby/bigdecimal_ext.rb
+++ src/main/resources/ruby/bigdecimal_ext.rb
@@ -2,9 +2,17 @@
 
 require 'bigdecimal'
 
+class BigDecimal
+  alias :to_fs :to_s
+end
+
 class Java::JavaMath::BigDecimal
   def to_bd
     BigDecimal(toString)
+  end
+
+  def to_fs(format = nil)
+    to_bd.to_fs(format || 'F')
   end
 
   def to_s
